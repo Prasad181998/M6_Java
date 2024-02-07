6eb384aa36e5c7759e647fd668fa1962578748de
Prasad181998 <154405568+Prasad181998@users.noreply.github.com>
2024-02-07 23:58:42 +0530

    1st day java program

---
+++ Print_Byte_Variable.java
@@ -0,0 +1,11 @@
+class Print_Byte_Variable // creating class
+{
+
+public static void main(String[] pbv) // creating main method
+{
+byte variable1=20; // declaration and intialization of byte variable
+
+System.out.println(variable1); //printing byte variable
+
+}
+}
---
+++ Print_Variable.java
@@ -0,0 +1,18 @@
+class Print_Variable
+{
+public static void main(String [] pv)
+{
+int variable1=10;
+short variable2=20;
+byte variable3=30;
+double variable4=40.10;
+long variable5=50;
+float variable6=60;
+System.out.println(variable1);
+System.out.println(variable2);
+System.out.println(variable3);
+System.out.println(variable4);
+System.out.println(variable5);
+System.out.println(variable6);
+}
+}
